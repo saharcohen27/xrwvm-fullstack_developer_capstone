c2f071ccd306745bfbac33ed8568880750ef37d0
Sahar Cohen <saharcohen456@gmail.com>
2024-07-15 07:02:23 -0400

    temp changes

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -26,4 +26,7 @@ urlpatterns = [
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     # path for add a review view
     path(route='add_review', view=views.add_review, name='add_review'),
+    # path for add a get_cars view
+    path(route='get_cars', view=views.get_cars, name='get_cars'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
